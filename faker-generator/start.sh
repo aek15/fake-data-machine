8c1bfd18eae11adc3b2decee6814af3cea91353f
alexanderklimenko <aeklmn@gmail.com>
2023-12-30 21:56:30 +0100

    fix start.sh

--- faker-generator/start.sh
+++ faker-generator/start.sh
@@ -4,10 +4,5 @@
 echo "Access the Fake Data Machine app at: http://localhost:8501"
 
 # Start Streamlit in the background and redirect its output
-nohup streamlit run app.py --server.address 0.0.0.0 --server.port 8501 > /dev/null 2>&1 &
+nohup streamlit run app.py --server.address 127.0.0.1 --server.port 8501 > /dev/null 2>&1 &
 
-# Wait a bit to ensure Streamlit starts
-sleep 5
-
-# Release the terminal
-exec "$@"
