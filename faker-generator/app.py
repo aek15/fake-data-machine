7671cf3bb91ffeb2fcef64099bfca6d649bc7f80
alexanderklimenko <aeklmn@gmail.com>
2023-12-30 17:08:19 +0100

    UI and Generator improvements

--- faker-generator/app.py
+++ faker-generator/app.py
@@ -3,22 +3,25 @@ import os
 from data_generator import generate_ddl, generate_dummy_data
 
 def main():
-    st.title("Dummy Data Generator")
+    st.title("Fake Data Machine")
 
     table_name = st.text_input("Table Name", "my_table")
-    num_columns = st.number_input("Number of Columns", min_value=1, max_value=10, value=3)
+    num_columns = st.number_input("Number of Columns", min_value=1, max_value=20, value=3)
 
     columns = []
     for i in range(int(num_columns)):
-        col_name = st.text_input(f"Name of column {i+1}", f"column_{i+1}")
-        col_type = st.selectbox(f"Data type of column {i+1}", ["VARCHAR", "INT"], index=0)
+        col1, col2 = st.columns(2)
+        with col1:
+            col_name = st.text_input(f"Column {i+1} Name", f"column_{i+1}")
+        with col2:
+            col_type = st.selectbox(f"Column {i+1} Type", ["VARCHAR", "INTEGER", "NUMBER", "BOOLEAN", "DATE", "TIME", "TIMESTAMP", "VARIANT"], index=0, key=f"type_{i+1}")
         columns.append({"name": col_name, "type": col_type})
 
     if st.button("Generate DDL"):
         ddl = generate_ddl(table_name, columns)
         st.text_area("DDL Statement", ddl, height=100)
 
-    num_rows = st.number_input("Number of Rows per File", min_value=1, max_value=1000, value=100)
+    num_rows = st.number_input("Number of Rows per File", min_value=1, max_value=1000000, value=100)
     num_files = st.number_input("Number of Files", min_value=1, max_value=10, value=1)
 
     if st.button("Generate Files"):
--- faker-generator/data_generator.py
+++ faker-generator/data_generator.py
@@ -1,14 +1,34 @@
 from faker import Faker
 import pandas as pd
+import json
+from datetime import datetime
+import random
 
 faker = Faker()
 
 def generate_ddl(table_name, columns):
-    ddl = f"CREATE OR REPLACE TABLE {table_name} (\n"
+    ddl = f"CREATE TABLE {table_name} (\n"
     ddl += ",\n".join([f"{col['name']} {col['type']}" for col in columns])
     ddl += "\n);"
     return ddl
 
 def generate_dummy_data(columns, num_rows):
-    data = {col['name']: [faker.pystr() if col['type'] == 'VARCHAR' else faker.random_number() for _ in range(num_rows)] for col in columns}
+    data = {}
+    for col in columns:
+        if col['type'] == 'VARCHAR':
+            data[col['name']] = [faker.word() for _ in range(num_rows)]
+        elif col['type'] == 'INTEGER':
+            data[col['name']] = [faker.random_number(digits=5, fix_len=True) for _ in range(num_rows)]
+        elif col['type'] == 'NUMBER':
+            data[col['name']] = [round(random.uniform(0, 10000), 2) for _ in range(num_rows)]
+        elif col['type'] == 'BOOLEAN':
+            data[col['name']] = [faker.boolean() for _ in range(num_rows)]
+        elif col['type'] == 'DATE':
+            data[col['name']] = [faker.date() for _ in range(num_rows)]
+        elif col['type'] == 'TIME':
+            data[col['name']] = [faker.time() for _ in range(num_rows)]
+        elif col['type'] == 'TIMESTAMP':
+            data[col['name']] = [datetime.now().strftime("%Y-%m-%d %H:%M:%S") for _ in range(num_rows)]
+        elif col['type'] == 'VARIANT':
+            data[col['name']] = [json.dumps({"key": faker.word(), "value": faker.word()}) for _ in range(num_rows)]
     return pd.DataFrame(data)
