93ddbc96b5abb097f1ca306beec675a0d7771f38
alexanderklimenko <aeklmn@gmail.com>
2023-12-31 15:15:50 +0100

    remove start script

--- faker-generator/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Echo the correct URL
-echo "Access the Fake Data Machine app at: http://localhost:8501"
-
-# Start Streamlit in the background and redirect its output
-nohup streamlit run app.py --server.address 127.0.0.1 --server.port 8501 > /dev/null 2>&1 &
-
